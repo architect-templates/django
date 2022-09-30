2c40fe644eb0def4b9c5af6f4f0de71676f2b7eb
Ryan Cahill <ryan.cahill444@gmail.com>
2022-09-30 11:40:47 -0400

    enabling css to load for remote deployments

--- architectexample/wsgi.py
+++ architectexample/wsgi.py
@@ -1,7 +1,9 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'architectexample.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/usr/src/app/static')
